87489369e1ec13e1401bae152d17a28f7b5d566f
Nguyễn Văn Hải <91068849+nvhaidev@users.noreply.github.com>
2022-01-22 19:40:47 +0700

    Create get_url_photos_fb-page.js

---
+++ get_url_photos_fb-page.js
@@ -0,0 +1,46 @@
+//https://www.facebook.com/pagenamdeptrai/photos/658452612246635
+javascript:(() => {
+    function findLink() {
+        const link = [];
+        document.querySelectorAll('a').forEach(((value, key) => {
+            if (document.querySelectorAll('a')[key].href) {
+                if (document.querySelectorAll('a')[key].href.split('/').length === 7) {
+                    if (document.querySelectorAll('a')[key].href.split('/')[4] === 'photos') {
+                        link.push(document.querySelectorAll('a')[key].href)
+                    }
+                }
+            }
+        }))
+        return link
+    }
+
+    function downloadLink(link) {
+        const uid = window.location.href.split('/')[3].split('?')[0];
+        const file = new Blob([JSON.stringify(link)], {type: 'text/plain'});
+        const url = window.URL.createObjectURL(file);
+        const b = document.createElement("a");
+        b.style.display = "none";
+        b.href = url;
+        b.setAttribute("download", `${uid}-phamthanhnam.com.txt`);
+        b.download = `${uid}-phamthanhnam.com.txt`;
+        document.body.appendChild(b);
+        b.click();
+        window.URL.revokeObjectURL(url);
+    }
+
+    var i = 0;
+
+    function pageScroll() {
+        i += 2;
+        const canvas = document.querySelectorAll('img').length;
+        window.scrollBy(0, 1000);
+        if (i > canvas) {
+            const link = findLink();
+            downloadLink(link);
+            return;
+        }
+        setTimeout(pageScroll, 100);
+    }
+
+    pageScroll()
+})()
